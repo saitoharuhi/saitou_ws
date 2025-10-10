d1dc4e4dbacdee2aac35c735eb96b62cf3c8a023
saitoharuhi <robotic.engineer.dream@gmail.com>
2025-10-10 17:06:49 +0900

    ros2 パッケージ作成

---
+++ ros2_ws/install/esp_radio_controlled_pkg/lib/python3.10/site-packages/esp_radio_controlled_pkg/image_reception_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from esp_radio_controlled_pkg.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros2_ws/src/esp_radio_controlled_pkg/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'esp_radio_controlled_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='altair',
+    maintainer_email='robotic.engineer.dream@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'image_reception_node = esp_radio_controlled_pkg.image_reception_node:main'
+        ],
+    },
+)
